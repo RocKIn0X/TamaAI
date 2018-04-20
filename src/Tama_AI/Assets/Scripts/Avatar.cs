587277b45dddef233db9647ffaaa4a16c733d79f
Suphanath Kaewboonruang <suphanathk@keemail.me>
2018-04-20 13:29:58 +0700

    Initial commit
    Showing Input co-ordination
    Avatar is drawn but do not move yet
    Tilemap is drawn

---
+++ src/Tama_AI/Assets/Scripts/Avatar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar : MonoBehaviour {
+
+  public Vector2 target;
+
+  // Use this for initialization
+  void Start () {
+    target = new Vector2(transform.position.x, transform.position.y);
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+    {
+      target = Input.GetTouch(0).position;
+    }
+    else if (Input.GetButtonDown("Tap"))
+    {
+      target = Input.mousePosition;
+    }
+  }
+}
---
+++ src/Tama_AI/Assets/Scripts/PosText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PosText : MonoBehaviour {
+
+  public Avatar avatar = null;
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (avatar != null)
+    {
+      GetComponent<Text>().text = avatar.target.x + " - " + avatar.target.y;
+    }
+  }
+}
